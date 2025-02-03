49fce93c00a51787ea9583cf54fa1eec0e926f63
S. M. Mohiuddin Khan Shiam <147746955+mohiuddin-khan-shiam@users.noreply.github.com>
2025-02-04 01:51:28 +0600

    Create hello_world.py

---
+++ Basics/hello_world.py
@@ -0,0 +1,10 @@
+# Basics/hello_world.py
+
+# This program prints "Hello, World!" to the console
+
+def main():
+    print("Hello, World!")
+
+# Entry point of the program
+if __name__ == "__main__":
+    main()
